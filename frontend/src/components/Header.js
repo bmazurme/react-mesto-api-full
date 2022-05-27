c0caa7a7c9db3690f242b742046e9543b1471b36
Bogdan <b@bmazur.ru>
2022-05-28 00:27:58 +0300

    fix: link

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,7 @@
-import logo from '../images/logo.svg';
 import NavBar from './NavBar';
 import React from 'react';
+// import { Link } from 'react-router-dom';
+// import { Switch } from "react-router-dom";
 
 function Header(props) {
   const [isOpen, setIsOpen] = React.useState(false);
@@ -10,14 +11,12 @@ function Header(props) {
 
   return (
     <header className={`header ${isOpen ? 'header_opened' : ''}`}>
-        <a href='/' 
-           className="logo" 
-           src={logo} 
-           alt="Логотип Место">
-        </a>
-        <NavBar {...props} 
-                handlerClick={handlerClick} 
-                isOpen={isOpen}/>
+      {/* <Switch> */}
+        <div to="/sign-in" className="logo" />
+      {/* </Switch> */}
+      <NavBar {...props} 
+              handlerClick={handlerClick} 
+              isOpen={isOpen}/>
     </header>
   );
 }
